bdbba96a5fc7a284de2a5303acd1a66560eeec6c
cipig <cipianon@gmail.com>
2021-12-20 12:30:22 +0000

    change tar source

--- ci_tools_atomic_dex/ci_scripts/linux_script.sh
+++ ci_tools_atomic_dex/ci_scripts/linux_script.sh
@@ -62,7 +62,8 @@ sudo make -j3 install
 cd ..
 
 ## tar
-wget https://ftp.gnu.org/gnu/tar/tar-1.32.tar.gz
+#wget https://ftp.gnu.org/gnu/tar/tar-1.32.tar.gz
+wget http://ftp.vim.org/ftp/gnu/tar/tar-1.32.tar.gz
 tar xvf tar-1.32.tar.gz
 cd tar-1.32
 export FORCE_UNSAFE_CONFIGURE=1
