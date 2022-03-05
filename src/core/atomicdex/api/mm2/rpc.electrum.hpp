6096025cfe60c1ed2d48bfd03c6f2b4e3df6bcbc
cipig <cipianon@gmail.com>
2022-03-05 23:42:18 +0000

    tx_history false

--- src/core/atomicdex/api/mm2/rpc.electrum.hpp
+++ src/core/atomicdex/api/mm2/rpc.electrum.hpp
@@ -31,7 +31,7 @@ namespace mm2::api
         std::vector<atomic_dex::electrum_server> servers;
         CoinType                                 coin_type;
         bool                                     is_testnet{false};
-        bool                                     with_tx_history{true};
+        bool                                     with_tx_history{false};
         const std::string                        testnet_qrc_swap_contract_address{"0xba8b71f3544b93e2f681f996da519a98ace0107a"};
         const std::string                        testnet_fallback_qrc_swap_contract_address{testnet_qrc_swap_contract_address};
         const std::string                        mainnet_qrc_swap_contract_address{"0x2f754733acd6d753731c00fee32cb484551cc15d"};
